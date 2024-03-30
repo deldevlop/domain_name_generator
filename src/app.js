4ef29e711dc705b89dfd3e1c718324c2a228bfbc
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-03-30 15:08:12 +0000

    Domain Generator Without Style

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+//thegreatjogger.com
+//thebigjogger.com
+//thegreatracoon.com
+//thebigracoon.com
+//ourgreatjogger.com
+//ourbigjoger.com
+//ourgreatracoon.com
+//ourbigracoon.com
+
+const generateDomain = (array1, array2, array3) => {
+  const generator = [];
+  if ((array1.lemgth = array2.length) && (array2.length = array3.length)) {
+    console.log("tableau de meme longueur");
+  }
+  let longueur = array1.length * array2.length * array2.length;
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      for (let k = 0; k < array3.length; k++) {
+        const domainName = array1[i] + array2[j] + array3[k] + ".com <br>";
+        generator.push(domainName);
+      }
+    }
+  }
+  return generator.join("");
+};
+console.log(generateDomain(pronoun, adj, noun));
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("generateDomain").innerHTML = generateDomain(
+    pronoun,
+    adj,
+    noun
+  );
 };
