86e00a2c5e842608498371e85ce9938e1c7179eb
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-03 22:20:19 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -37,7 +37,6 @@ console.log(generateDomain(pronoun, adj, noun));
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
   document.getElementById("generateDomain").innerHTML = generateDomain(
     pronoun,
     adj,
